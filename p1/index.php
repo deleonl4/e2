5c0e1c8b188ea01fb0b2363a2a0159cc92866dda
leslie de leon <deleonl@leslies-MacBook-Pro.local>
2020-09-23 22:37:09 -0400

    Update p1

--- p1/index.php
+++ p1/index.php
@@ -7,3 +7,14 @@
     <h1>Project 1</h1>
 </body>
 </html>
+
+
+<VirtualHost *:80>
+    ServerName e2p1.loc
+    DocumentRoot /path/to/htdocs/e2/p1
+    <Directory /Applications/XAMPP/xamppfiles/htdocs/e2/p1>
+        AllowOverride All
+        Options -Indexes
+        Require all granted
+    </Directory>
+</VirtualHost>
