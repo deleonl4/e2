b9951d52c769fac0e9952c15b45ba7bc449db9a9
leslie de leon <deleonl@leslies-MacBook-Pro.local>
2020-09-24 23:03:12 -0400

    Updates to index, index view, and bank files

--- p1/index.php
+++ p1/index.php
@@ -1,20 +1,7 @@
-<!doctype html>
-<html lang='en'>
-<head><title>Project1</title>
-    <meta charset='utf-8'>
-</head>
-<body>
-    <h1>Project 1</h1>
-</body>
-</html>
 
+<?php require 'index-view.php';
 
-<VirtualHost *:80>
-    ServerName e2p1.loc
-    DocumentRoot /path/to/htdocs/e2/p1
-    <Directory /Applications/XAMPP/xamppfiles/htdocs/e2/p1>
-        AllowOverride All
-        Options -Indexes
-        Require all granted
-    </Directory>
-</VirtualHost>
+$name =
+echo gettype ($name);
+
+?>
