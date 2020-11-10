07d48300969cb2011c87ee9fe2f9592df10a22b4
leslie de leon <deleonl@leslies-MacBook-Pro.local>
2020-11-10 07:46:58 -0500

    My App week 10

--- MyApp/app/Controllers/AppController.php
+++ MyApp/app/Controllers/AppController.php
@@ -14,4 +14,22 @@ class AppController extends Controller
             'welcome' => $welcomes[array_rand($welcomes)]
         ]);
     }
+
+    /**
+     * This method is triggered by the route "/contact"
+     */
+    public function contact()
+    {
+        //return $this->app->view('contact');
+        return "zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area";
+
+    }
+
+    /**
+     * This method is triggered by the route "/about"
+     */
+    public function about()
+    {
+        return "zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area";
+    }
 }
