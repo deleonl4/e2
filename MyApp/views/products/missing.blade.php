38881c9e7aa94a402b344dfc988f2514487fb1ae
leslie de leon <deleonl@leslies-MacBook-Pro.local>
2020-11-11 19:46:56 -0500

    Missing.blade.php file

---
+++ MyApp/views/products/missing.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+
+    <title>Product Not Found</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <h2>Product Not Found</h2>
+    <a href='{{ $app->config('app.url') }}'>{{ $app->config('app.url') }}</a>
+    <p> Uh oh - we were not able to find the product you were looking for. </p>
+    <a href="http://myapp.loc/products" Check out our selection of products...>
+
+</body>
+
+</html>
